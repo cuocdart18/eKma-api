fcdf454730ab54381d0a37d7dab6543ee93e436e
ddaa12 <ddaduyanh123@gmail.com>
2023-01-18 17:28:23 +0700

    init project

---
+++ server.js
@@ -0,0 +1,163 @@
+/** @format */
+
+const axios = require('axios').default
+const axiosCookieJarSupport = require('axios-cookiejar-support').default
+const tough = require('tough-cookie')
+const md5 = require('md5')
+const qs = require('query-string')
+const cheerio = require('cheerio')
+
+const { parseInitialFormData, parseSelector } = require('./utils')
+const listSchedule = require('./schedule')
+
+const express = require('express')
+const server = express()
+
+const loginUrl = 'http://qldt.actvn.edu.vn/CMCSoft.IU.Web.Info/Login.aspx'
+const studentProfileUrl = 'http://qldt.actvn.edu.vn/CMCSoft.IU.Web.Info/StudentProfileNew/HoSoSinhVien.aspx'
+
+
+axiosCookieJarSupport(axios)
+const cookieJar = new tough.CookieJar()
+
+axios.defaults.withCredentials = true
+axios.defaults.crossdomain = true
+axios.defaults.jar = cookieJar
+
+async function main(req) {
+    const username = req.username
+    const password = req.password
+    //const username = req.query.username || (req.body && req.body.username)
+    //const password = req.query.password || (req.body && req.body.password)
+
+    if (!username || !password) {
+        console.log("Missing items!");
+        return JSON.stringify("Missing items!");
+    }
+
+    console.log('Login With ID:', username);
+
+    try {
+        const loginGet = await axios.get(loginUrl, { withCredentials: true, jar: cookieJar })
+
+        let $ = cheerio.load(loginGet.data)
+
+        const formData = {
+            ...parseInitialFormData($),
+            ...parseSelector($),
+            txtUserName: username,
+            txtPassword: md5(password),
+            btnSubmit: 'Đăng nhập',
+        }
+
+        const form = qs.stringify(formData)
+
+        const config = {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/76.0.114 Chrome/70.0.3538.114 Safari/537.36',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            withCredentials: true,
+            jar: cookieJar,
+        }
+
+        const loginPost = await axios.post(loginUrl, form, config)
+
+        $ = cheerio.load(loginPost.data)
+        const userFullName = $('#PageHeader1_lblUserFullName').text().toLowerCase()
+        const wrongPass = $('#lblErrorInfo').text()
+
+        if (wrongPass == 'Bạn đã nhập sai tên hoặc mật khẩu!' || wrongPass == 'Tên đăng nhập không đúng!') {
+            console.log("Sai ten dang nhap hoac mat khau")
+            return JSON.stringify("Sai ten dang nhap hoac mat khau");
+        }
+
+        if (userFullName == 'khách') {
+            console.log("Please login again!")
+            return JSON.stringify("Please login again!");
+        }
+
+        let schedule = await listSchedule(cookieJar)
+
+        if (schedule.code != 200) {
+            console.log(schedule.message)
+        } else {
+            const res = await axios.get(studentProfileUrl, { withCredentials: true, jar: cookieJar })
+
+            $ = cheerio.load(res.data)
+            const displayName = ($('input[name="txtHoDem"]').val() || '') + ' ' + ($('input[name="txtTen"]').val() || '')
+            const studentCode = $('input[name="txtMaSV"]').val() || ''
+            const gender = $('select[name="drpGioiTinh"] > option[selected]').text()
+            const birthday = $('input[name="txtNgaySinh"]').val() || ''
+            const information = {
+                displayName,
+                studentCode,
+                gender,
+                birthday,
+            }
+
+
+            var periods = []
+            schedule.data.forEach(displaySchedule)
+
+
+            function displaySchedule(item, index, arr) {
+
+                var period = {
+                    id: index,
+                    day: item.day,
+                    subjectCode: item.subjectCode,
+                    subjectName: item.subjectName,
+                    className: item.className,
+                    teacher: item.teacher,
+                    lesson: item.lesson,
+                    room: item.room
+                }
+
+                periods[index] = period
+
+            }
+
+            var result = {
+                studentCode: information.studentCode,
+                studentName: information.displayName,
+                gender: information.gender,
+                birthday: information.birthday,
+                scheduleMessage: schedule.message,
+                periods: periods
+            }
+
+            const json = await JSON.stringify(result)
+            return json
+
+        }
+    } catch (e) {
+        console.log(e)
+
+        console.log("Error: ", e)
+    }
+}
+
+
+
+
+server.get("/schedule", function (req, res) {
+    var username = req.query.username
+    var password = req.query.password
+
+    const account = {
+        username: username,
+        password: password
+    }
+    console.log(`query: username: ${account.username} & password: ${account.password}`)
+    let result = main(account)
+    result.then(function (r) {
+        console.log(r)
+        res.send(r)
+        res.end()
+
+    })
+    
+})
+
+server.listen(3000)
