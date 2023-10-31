5f01ea39379ee28ba543f740456383cdfc549234
Cuoc Dat <ngoquocdat203@gmail.com>
2023-10-31 23:39:10 +0700

    add: base socket.io feature

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
-const server = express()
+const app = express()
 
 var bodyParser = require('body-parser');
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const socketServer = require('http').createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(socketServer);
+
+const portSocket = 3000
+socketServer.listen(portSocket, () => {
+    console.log('socketServer listening on port = ' + portSocket);
+});
+
+const portApp = process.env.PORT || 9001
+app.listen(portApp, () => {
+    console.log('app listening on port = ' + portApp);
+});
 
 const routes = require('./routes/routes')
-
-routes(server)
-
-const port = process.env.PORT || 9001
-server.listen(port)
+routes(app, io)
